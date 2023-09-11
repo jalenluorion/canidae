407af2dfff38f3d71fd5df085ebd692f8648666a
Jalen Lu <jalenlu2000@icloud.com>
2023-09-11 00:13:27 -0600

    timerr

--- src/ViewsTop/Timer.js
+++ src/ViewsTop/Timer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Timer.css';
 import './ViewsTop.css';
-import TimePicker from 'react-time-picker'; // Import react-time-picker
+import TimePicker from 'react-time-picker';
 import 'react-time-picker/dist/TimePicker.css';
 import 'react-clock/dist/Clock.css';
 
@@ -9,22 +9,23 @@ const TimerView = () => {
   const [time, setTime] = useState(1500); // Initial time in seconds (25 minutes)
   const [timerLabel, setTimerLabel] = useState('Study');
   const [isActive, setIsActive] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-  const [isTimePickerOpen, setTimePickerOpen] = useState(false); // State to control time picker modal
+  const [isPaused, setIsPaused] = useState(true);
+  const [isTimePickerOpen, setTimePickerOpen] = useState(false);
 
   useEffect(() => {
     let interval;
 
-    if (isActive && time > 0) {
+    if (!isPaused && time > 0) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
       }, 1000);
-    } else if (!isActive && time !== 0) {
+    } else if (isPaused && time !== 0) {
       clearInterval(interval);
+      sendNotification();
     }
 
     return () => clearInterval(interval);
-  }, [isActive, time]);
+  }, [isPaused, time]);
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -32,22 +33,18 @@ const TimerView = () => {
     return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
   };
 
-  const toggleTimer = () => {
-    if (!isActive && time === 0) {
-      setTime(1500); // Reset time to 25 minutes when starting after completion
-    }
-    setIsActive(!isActive);
+  const startTimer = () => {
+    setIsActive(true);
     setIsPaused(false);
   };
 
-  const pauseTimer = () => {
-    setIsActive(false);
-    setIsPaused(true);
+  const togglePause = () => {
+    setIsPaused(!isPaused);
   };
 
   const resetTimer = () => {
     setIsActive(false);
-    setIsPaused(false);
+    setIsPaused(true);
     setTime(1500); // Reset time to 25 minutes
     setTimerLabel('Study');
   };
@@ -56,29 +53,38 @@ const TimerView = () => {
     setTimePickerOpen(!isTimePickerOpen);
   };
 
-  const closeTimePicker = () => {
-    setTimePickerOpen(false);
-  };
-
   const updateTimePickerValue = (newTime) => {
-    // Convert the selected time from the time picker to seconds and update the state
     const [hours, minutes] = newTime.split(':');
     const newSeconds = parseInt(hours, 10) * 3600 + parseInt(minutes, 10) * 60;
     setTime(newSeconds);
-    closeTimePicker();
+    setTimePickerOpen(false);
   };
 
-  const startButton = (
-    <button className={`btn ${isActive ? 'btn-pause' : 'btn-start'}`} onClick={toggleTimer}>
-      {isActive ? 'Pause' : 'Start'}
-    </button>
-  );
+  const sendNotification = () => {
+    if (Notification.permission === 'granted') {
+      new Notification('Timer Ended', { body: 'Your timer has ended.' });
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === 'granted') {
+          new Notification('Timer Ended', { body: 'Your timer has ended.' });
+        }
+      });
+    }
+  };
 
   const pauseResumeButtons = (
     <div className="btn-group">
-      <button className="btn btn-pause" onClick={pauseTimer}>Pause</button>
+      <button className="btn btn-pause" onClick={togglePause}>
+        {isPaused ? 'Resume' : 'Pause'}
+      </button>
       <button className="btn btn-reset" onClick={resetTimer}>Reset</button>
     </div>
+  );
+
+  const startButton = (
+    <button className="btn btn-start" onClick={startTimer}>
+      Start
+    </button>
   );
 
   return (
@@ -88,34 +94,37 @@ const TimerView = () => {
           <h1>Study Timer</h1>
         </div>
         {isTimePickerOpen && (
-        <div className="menu-dropdown-top red-accent">
-          <TimePicker
-            onChange={updateTimePickerValue}
-            value={formatTime(time / 60)}
-            clockIcon={null} // Disable the clock icon
-            maxDetail="minute" // Limit the view to hours and minutes
-            clearIcon={null}
-            format="HH:mm" // Display hours and minutes in 24-hour format
-            hourPlaceholder="hh"
-            minutePlaceholder="mm"
-            showLeadingZeros={true}
-            disableClock={true} // Disable the clock view
-            isOpen={true}
-          />
-        </div>
-      )}
+          <div className="menu-dropdown-top red-accent">
+            <TimePicker
+              onChange={updateTimePickerValue}
+              value={formatTime(time / 60)}
+              clockIcon={null}
+              maxDetail="minute"
+              clearIcon={null}
+              format="HH:mm:ss"
+              hourPlaceholder="hh"
+              minutePlaceholder="mm"
+              showLeadingZeros={true}
+              disableClock={true}
+              isOpen={true}
+            />
+          </div>
+        )}
       </div>
       <div className="timer-body">
         <div className="timer-display" onClick={toggleTimePicker}>
           {formatTime(time)}
         </div>
         <div className="button-container">
-          {isActive || isPaused ? pauseResumeButtons : startButton}
+          {isActive ? pauseResumeButtons : startButton}
         </div>
         <div className="timer-label">
-          <button className={`btn ${timerLabel === 'Study' ? 'btn-active' : ''}`} onClick={() => setTimerLabel('Study')}>Study</button>
-          <button className={`btn ${timerLabel === 'Rest' ? 'btn-active' : ''}`} onClick={() => setTimerLabel('Rest')}>Rest</button>
-          {/* Add custom label functionality */}
+          <button className={`btn ${timerLabel === 'Study' ? 'btn-active' : ''}`} onClick={() => setTimerLabel('Study')}>
+            Study
+          </button>
+          <button className={`btn ${timerLabel === 'Rest' ? 'btn-active' : ''}`} onClick={() => setTimerLabel('Rest')}>
+            Rest
+          </button>
         </div>
       </div>
     </div>

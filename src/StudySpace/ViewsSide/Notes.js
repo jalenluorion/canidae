e5af037708665347dbc70b142ff0b23fde8a7859
Jalen Lu <jalenlu2000@icloud.com>
2023-09-21 23:54:19 +0000

    ez new feature

--- src/StudySpace/ViewsSide/Notes.js
+++ src/StudySpace/ViewsSide/Notes.js
@@ -5,20 +5,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 import './Notes.css';
-import './ViewsSide.css'
+import './ViewsSide.css';
 
 const NoteView = () => {
-  const [note, setNote] = useState('');
+  const [notes, setNotes] = useState({});
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [title, setTitle] = useState('');
   const [buttonClicked, setButtonClicked] = useState(false);
+  const [selectedPeriod, setSelectedPeriod] = useState('8');
 
   const popupRef = useRef(null);
 
   useEffect(() => {
-    const savedNote = Cookies.get('savedNote');
-    if (savedNote) {
-      setNote(savedNote);
+    // Load saved notes from cookies
+    const savedNotes = Cookies.get('savedNotes');
+    if (savedNotes) {
+      setNotes(JSON.parse(savedNotes));
     }
   }, []);
 
@@ -39,8 +41,8 @@ const NoteView = () => {
 
   const handleNoteChange = (e) => {
     const updatedNote = e.target.value;
-    setNote(updatedNote);
-    Cookies.set('savedNote', updatedNote, { expires: 7 });
+    setNotes({ ...notes, [selectedPeriod]: updatedNote });
+    Cookies.set('savedNotes', JSON.stringify({ ...notes, [selectedPeriod]: updatedNote }), { expires: 7 });
   };
 
   const handleDownload = (e) => {
@@ -61,7 +63,7 @@ const NoteView = () => {
     e.stopPropagation();
 
     const downloadTitle = title.trim() ? title : 'Notes';
-    const blob = new Blob([note], { type: 'text/plain' });
+    const blob = new Blob([notes[selectedPeriod]], { type: 'text/plain' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
@@ -71,6 +73,34 @@ const NoteView = () => {
 
     setIsPopupOpen(false);
     setTitle('');
+  };
+
+
+  const getPeriodColor = (period) => {
+    switch (period) {
+      case '1':
+        return 'red';
+      case '2':
+        return 'darkorange';
+      case '3':
+        return 'yellow';
+      case '4':
+        return 'green';
+      case '5':
+        return 'blue';
+      case '6':
+        return 'indigo';
+      case '7':
+        return 'violet';
+      case '8':
+        return 'gray';
+      default:
+        return 'gray';
+    }
+  };
+
+  const handlePeriodChange = (e) => {
+    setSelectedPeriod(e.target.value);
   };
 
   return (
@@ -100,11 +130,28 @@ const NoteView = () => {
       </div>
 
       <textarea
-        value={note}
+        value={notes[selectedPeriod] || ''} // Display the note for the selected period
         className="text-area main-body"
         onChange={handleNoteChange}
         placeholder="Write your notes here..."
       />
+      <div className="input-container centered">
+        <select
+          value={selectedPeriod}
+          onChange={handlePeriodChange}
+          className="period-select notes"
+          style={{ backgroundColor: getPeriodColor(selectedPeriod), color: getPeriodColor(selectedPeriod) === 'yellow' || getPeriodColor(selectedPeriod) === 'pink' ? 'black' : 'white' }}
+        >
+          <option style={{ backgroundColor: getPeriodColor('1') }} value="1">Period 1</option>
+          <option style={{ backgroundColor: getPeriodColor('2') }} value="2">Period 2</option>
+          <option style={{ backgroundColor: getPeriodColor('3') }} value="3">Period 3</option>
+          <option style={{ backgroundColor: getPeriodColor('4') }} value="4">Period 4</option>
+          <option style={{ backgroundColor: getPeriodColor('5') }} value="5">Period 5</option>
+          <option style={{ backgroundColor: getPeriodColor('6') }} value="6">Period 6</option>
+          <option style={{ backgroundColor: getPeriodColor('7') }} value="7">Period 7</option>
+          <option style={{ backgroundColor: getPeriodColor('8') }} value="8">Other</option>
+        </select>
+      </div>
     </div>
   );
 };
